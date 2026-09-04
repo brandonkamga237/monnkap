ce89130504d71d39866f02cc231c07fe76db026c
Brandon Kamga <brandoniscoding.dev@gmail.com>
2026-09-04 19:38:28 +0100

    feat:define Transaction class

---
+++ README.md
@@ -0,0 +1,9 @@
+# Track my financial transaction
+
+## Features
+
+- Create transaction
+- Update transaction
+- Delete trnasction
+- Search transaction
+- List trasactions
---
+++ Transaction.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Transaction {
+
+    private UUID num;
+    private String description;
+    private BigDecimal amount;
+    private TransactionType transactionType;
+
+
+}
