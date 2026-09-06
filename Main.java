770247804b65b1c099d013500f2212a5e3a54529
Brandon Kamga <brandoniscoding.dev@gmail.com>
2026-09-06 23:46:41 +0100

    make first transaction tests

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello monnkap's user !");
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        transactions.add(new Transaction("Take out the trash", new BigDecimal("10.00"), TransactionType.OUT));
+        transactions.add(new Transaction("Get groceries", new BigDecimal("50.00"), TransactionType.OUT));
+
+        System.out.println("Transactions:");
+        for (Transaction transaction : transactions) {
+            System.out.println("- " + transaction.getDescription() + ": " + transaction.getAmount());
+        }
     }
 }
